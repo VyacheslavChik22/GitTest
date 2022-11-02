cce04b37fbf37b81f8eee81265bba0b3f8930c1e
asyof <asyofka@mail.ru>
2022-11-02 13:42:36 +0300

    Get random name

--- src/GitTest.java
+++ src/GitTest.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class GitTest {
+public class GitTest {
+    public static void main(String[] args) {
+        String[] names = {
+                "Pavel",
+                "Sergey",
+                "Svetlana",
+                "Artur",
+                "Valentina"
+        };
+        int randomNum = (int) (Math.random() * names.length + 1);
+        for (int i = 0; i < names.length; i++) {
+            if (i == randomNum - 1) {
+                System.out.println(names[i]);
+            }
+        }
+    }
 }
