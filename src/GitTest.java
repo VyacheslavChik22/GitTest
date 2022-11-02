f95a118caa514ef85d3966b7d09e975a6ea8db0f
asyof <asyofka@mail.ru>
2022-11-02 14:07:08 +0300

    Get all names and random name in console

--- src/GitTest.java
+++ src/GitTest.java
@@ -1,3 +1,5 @@
+
+
 public class GitTest {
     public static void main(String[] args) {
         String[] names = {
@@ -7,8 +9,14 @@ public class GitTest {
                 "Artur",
                 "Valentina"
         };
+        System.out.println();
         int randomNum = (int) (Math.random() * names.length + 1);
         for (int i = 0; i < names.length; i++) {
+            if(i< names.length-1){
+                System.out.print(names[i]+", ");
+            }else {
+                System.out.print(names[i]+".\n");
+            }
             if (i == randomNum - 1) {
                 System.out.println(names[i]);
             }
